12f2d3422aa0b924e587dbba28db4736e27b658f
calder-c <163036584+calder-c@users.noreply.github.com>
2025-12-08 15:44:00 +0000

    Added edge collision checking

--- src/body.h
+++ src/body.h
@@ -1,8 +1,5 @@
-#include <complex>
-
 #include "definitions.h"
 #include "include.h"
-#include "utils.h"
 class Point {
 public:
     sf::Vector2f currentPos, oldPos, acc;
@@ -10,15 +7,44 @@ public:
     float radius;
     Point(sf::Vector2f currentPos_, sf::Vector2f oldPos_, sf::Vector2f acc_, float radius_) : currentPos(currentPos_), oldPos(oldPos_), acc(acc_), radius(radius_){
         shape.setRadius(radius);
+        shape.setOrigin(sf::Vector2f(radius, radius));
+    }
+    Point(sf::Vector2f pos_, sf::Vector2f acc_, float radius_) : currentPos(pos_), oldPos(pos_), acc(acc_), radius(radius_){
+        shape.setRadius(radius);
+        shape.setOrigin(sf::Vector2f(radius, radius));
+    }
+    void freeze() {
+        oldPos = currentPos;
+        acc = sf::Vector2f(0, 0);
     }
     void update(float dt) {
         sf::Vector2f buf = currentPos;
-        currentPos = currentPos + (currentPos - oldPos) + acc*(dt*dt);
+        sf::Vector2f diffPos = (currentPos - oldPos);
+        currentPos = currentPos + diffPos + acc*(dt*dt);
         oldPos = buf;
         shape.setPosition(currentPos);
+    }
+    void applyScreenConstraint() {
+        sf::Vector2f diffPos = (currentPos - oldPos);
+        if (currentPos.x < 0) {
+            currentPos.x = 0;
+            oldPos.x = (currentPos + diffPos).x;
+        } else if (currentPos.x > SCREEN_WIDTH) {
+            currentPos.x = SCREEN_WIDTH;
+            oldPos.x = (currentPos + diffPos).x;
+        }else if (currentPos.y < 0) {
+            currentPos.y = 0;
+            oldPos.y = (currentPos + diffPos).y;
+        } else if (currentPos.y > SCREEN_HEIGHT) {
+            currentPos.y = SCREEN_HEIGHT;
+            oldPos.y = (currentPos + diffPos).y;
+        }
+
     }
     void draw(sf::RenderWindow & window) {
 
         window.draw(shape);
     }
+
 };
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,45 @@
 #include "body.h"
 #include "include.h"
 int main() {
-    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Physics Simulation Personal Project");
-    sf::Clock deltaClock;
-    sf::Time dt = sf::milliseconds(16.6667);
-    Point obj1{sf::Vector2f(1, 1), sf::Vector2f(0.9, 0.9), sf::Vector2f(1, 0), 100};
+    sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}), "Physics Simulation Personal Project");
+    window.setFramerateLimit(0);
+    window.setVerticalSyncEnabled(false);
+    sf::Clock clock;
+    //sf::Time dt = sf::milliseconds(16.6667);
+    const float dt = 1.0/60.0;
+    float accumulator = 0.0f;
+    std::vector<Point> objList{};
+    objList.push_back(Point{sf::Vector2f(200, 300), sf::Vector2f(1, 98), 10});
+    objList.push_back(Point{sf::Vector2f(250, 200), sf::Vector2f(1, 98), 10});
+    objList.push_back(Point{sf::Vector2f(220, 100), sf::Vector2f(1, 98), 10});
+    objList.push_back(Point{sf::Vector2f(210, 0), sf::Vector2f(1, 98), 10});
+    objList.push_back(Point{sf::Vector2f(0, 400), sf::Vector2f(1, 98), 10});
+    objList.push_back(Point{sf::Vector2f(60, 400), sf::Vector2f(61, 400), sf::Vector2f(1, 98), 10});
     while (window.isOpen())
     {
-        window.clear(sf::Color::Black);
-        obj1.update(dt.asSeconds());
-
+        float frameTime = clock.restart().asSeconds();
+        accumulator += frameTime;
         while (const std::optional event = window.pollEvent())
         {
             //checks for close event
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-        obj1.draw(window);
+        while (accumulator >= dt) {
+            for (auto & obj : objList) {
+                obj.applyScreenConstraint();
+                obj.update(dt);
+            }
+            accumulator -= dt;
+        }
+        window.clear();
+        for (auto & obj : objList) {
+
+            obj.draw(window);
+        }
+
         window.display();
-        dt = deltaClock.restart();
+
 
     }
 }
